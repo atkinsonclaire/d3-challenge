9c74ce1f49c13f8fb1c9903186173a111afa09e8
atkinsonclaire <78178214+atkinsonclaire@users.noreply.github.com>
2021-06-21 19:36:02 -0400

    Add files via upload

--- docs/app.js
+++ docs/app.js
@@ -12,7 +12,6 @@ var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
 var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
 
 var svg = d3
-  .select("body")
   .select("scatter")
   .append("svg")
   .attr("height", svgHeight)
@@ -21,7 +20,7 @@ var svg = d3
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
 
-d3.csv("data/data.csv").then(function(newsData) {
+d3.csv("data.csv").then(function(newsData) {
 
     var yScale = d3.scaleLinear()
       .domain([0, d3.max(newsData.healthcare)])
